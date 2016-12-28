9bff75ee86794013d155aaa9e1a0b43d799a4cad
ab396356 <ab396356@users.noreply.github.com>
2016-12-28 10:56:40 +0200

    Update weasel.cpp

--- cpp_misc/weasel.cpp
+++ cpp_misc/weasel.cpp
@@ -4,13 +4,15 @@
 //  https://en.wikipedia.org/wiki/Weasel_program
 //
 // The Weasel program is a thought experiment and a very rough simulation of
-// evolution and natural selection, by starting with a random string of letters
-// and gradually mutating them into the text "methinks it is like a weasel".
+// evolution, by starting with a random string of letters and gradually
+// "evolving" them into the text "methinks it is like a weasel".
+//
 // The program can be easily modified to generate text that satisfies other
 // criteria, e.g. being a palindrome, thus not choosing one particular output.
 //
-// The starting text is random and is mutated randomly (evolution) and it is
-// then scored for suitability (natural selection).
+// The starting text is random and is mutated randomly (mutation) and it is
+// then scored for suitability (natural selection), resulting in a non-random
+// output (evolution).
 //
 
 #include <algorithm>
