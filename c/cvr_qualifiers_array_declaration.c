d2bd7fbe3c117eedd179dbd949406404f23ddba1
ab396356 <ab396356@users.noreply.github.com>
2016-12-30 21:42:18 +0200

    Update cvr_qualifiers_array_declaration.c

--- c/cvr_qualifiers_array_declaration.c
+++ c/cvr_qualifiers_array_declaration.c
@@ -9,21 +9,21 @@
 //  http://c-faq.com/aryptr/aryptrparam.html
 //
 
-static void     func0(int arr[]);                   // same as
-// static void  func0(int arr[10]);                 // same as
-// static void  func0(int *arr);
+extern void     func0(int arr[]);                   // same as
+// extern void  func0(int arr[10]);                 // same as
+// extern void  func0(int *arr);
 
-static void     func1(int arr[const]);              // same as
-// static void  func1(int * const arr);
+extern void     func1(int arr[const]);              // same as
+// extern void  func1(int * const arr);
 
-static void     func2(int arr[const volatile]);     // same as
-// static void  func2(int * const volatile arr);
+extern void     func2(int arr[const volatile]);     // same as
+// extern void  func2(int * const volatile arr);
 
-static void     func3(int arr[restrict]);           // same as
-// static void  func3(int * restrict arr);
+extern void     func3(int arr[restrict]);           // same as
+// extern void  func3(int * restrict arr);
 
-static void     func4(int arr[restrict static 10]); // same as
-// static void  func4(int * restrict arr);          // with optimization hints
+extern void     func4(int arr[restrict static 10]); // same as
+// extern void  func4(int * restrict arr);          // with optimization hints
 
 int main(void)
 {
